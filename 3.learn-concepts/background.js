78d2876896a19fd2622a6124942b9e0ec885fd42
Jaber Khan <md.jaberkhan66@gmail.com>
2026-06-02 06:28:12 +0600

    practices

--- 3.learn-concepts/background.js
+++ 3.learn-concepts/background.js
@@ -1,26 +1,32 @@
 // this runs when Chrome starts and satys running in backgorund
-chrome.runtime.onInstalled.addEventListener(() => {
+chrome.runtime.onInstalled.addListener(() => {
     console.log("Extension installed");
 })
 
 
-// listen for messages from other extensions OR content scripts
+// listen for messages from other extensions
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log("Received message:", message);
-    console.log("From sender:", sender);
-    
-    // send a response back
-    sendResponse({ message: "Message received in background script" });
-    
-    // Return true to indicate you want to send a response asynchronously
-    return true;
+    console.log("Received message from another extension:", message);
 });
 
 // listen for click events on the extension's browser action
 chrome.action.onClicked.addListener((tab) => {
-    console.log("Browser action clicked:", tab);
-    console.log("URL:", tab.url);
-    console.log("Title:", tab.title);
-    console.log("Tab ID:", tab.id);
-    console.log("Window ID:", tab.windowId);
-})
+    console.log("Browser action clicked:", tab, tab.url, tab.title, tab.id, tab.windowId);
+});
+
+// listen for messages from content script
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log("Received message from content script:", message);
+    // send a response back to the content script
+    sendResponse({ message: "Message received in background script" });
+});
+
+// listen for context menu click events
+chrome.contextMenus.create({
+    id: "sampleContextMenu",
+    title: "Sample Context Menu",
+    contexts: ["all"],
+    onclick: (info, tab) => {
+        console.log("Context menu item clicked:", info, tab);
+    }
+});
